7e88e6a29d97f13f29c33c710d489c10b8958e41
Joulain <vincent.joulain@viacesi.fr>
2021-06-24 11:25:35 +0200

    FEAT | ajout deploy users
    Ajout deploy pour kubernetes

--- back/microservice/Users/routes/users.js
+++ back/microservice/Users/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('User api');
+    res.send('User api !');
 });
 
 module.exports = router;
