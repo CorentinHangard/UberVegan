33da90c5384158cb095521e44f337f649ad96e19
CharlesSmrd <charles.semard@viacesi.fr>
2021-07-01 15:28:30 +0200

    FIX | Components

--- front2/src/views/developpeur/Developpeur.vue
+++ front2/src/views/developpeur/Developpeur.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card max-width="1000" class="mx-auto pa-5" align="center" flat>
+  <v-card max-width="1500" class="mx-auto pa-5" align="center" flat>
     <v-row justify="center" align="center">
       <v-container>
         <h1 class="center">Composant pour developpeur</h1>
@@ -11,21 +11,37 @@
 
           <br />
           <v-row>
+            <v-col></v-col>
             <v-col>
-              <span
-                >Voici le composant Menu permettant l'affichage des menus dans
-                l'application. Pour le télécharger veuillez entrer la commande
-                suivante dans votre commande prompt : npm i UberVeganMenu
-              </span>
+              <div>
+                <span
+                  >Voici le composant Menu permettant l'affichage des menus dans
+                  l'application. Pour le télécharger veuillez entrer la commande
+                  suivante dans votre command prompt :
+                  <strong>npm i uberveganmenugroupetroiscesirouen</strong>
+                </span>
+              </div>
             </v-col>
-
-            <Menu
-              v-bind="restaurants"
-              :name="restaurants[0].name"
-              :description="restaurants[0].description"
-              :id="restaurants[0].id"
-              :price="restaurants[0].cod"
-            />
+            <v-col>
+              <Menu
+                v-bind="restaurants"
+                :name="restaurants[0].name"
+                :description="restaurants[0].description"
+                :id="restaurants[0].id"
+                :price="restaurants[0].cod"
+              />
+            </v-col>
+            <v-col></v-col>
+          </v-row>
+          <br />
+          <br />
+          <v-row>
+            <iframe
+              id="myFrame"
+              width="1500"
+              height="1500"
+              src="https://npm-stat.com/charts.html?package=uberveganmenugroupetroiscesirouen&from=2020-06-30&to=2021-06-30"
+            ></iframe>
           </v-row>
         </div>
       </v-container>
@@ -33,11 +49,18 @@
   </v-card>
 </template>
 <script>
-//import Menu from "@/components/Menu.vue";
-import Menu from "uberveganmenugroupetroiscesirouen";
+import Menu from "@/components/Menu.vue";
+// import MenuImp from "uberveganmenugroupetroiscesirouen"
 
 export default {
   components: { Menu },
+
+  mounted() {
+    var iframe = document.getElementById("myFrame");
+    var elmnt = iframe.contentWindow.document.getElementsByTagName("H1")[0];
+    elmnt.style.display = "none";
+  },
+
   data() {
     return {
       restaurants: [
@@ -48,6 +71,7 @@ export default {
           cod: "25",
         },
       ],
+      wk: "",
     };
   },
 };
